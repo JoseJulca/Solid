f9356e1c85d6447e85c05cbb1a119445bbe7c0cf
JoseJulca <63326974+JoseJulca@users.noreply.github.com>
2026-06-12 20:48:20 -0500

    se agregaron las clases de separacion

--- Servicio-Solid/Servicio-Solid/Program.cs
+++ Servicio-Solid/Servicio-Solid/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Servicio_Solid;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var sistema = new SistemaReservaCanchaSinSolid();
+        sistema.RealizarReserva("Juan Perez", "Cancha7", 2, "VIP", "Email");
+    }
+
+   
+}
---
+++ Servicio-Solid/Servicio-Solid/SistemaReservaCanchaSinSolid.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicio_Solid
+{
+    public  class SistemaReservaCanchaSinSolid
+    {
+        public SistemaReservaCanchaSinSolid()
+        {
+
+        }
+
+        public void RealizarReserva(string nombreCliente, string tipoCancha, int horas, string tipoCliente, string metodoNotificacion)
+        {
+            // 1. Validación
+            if (string.IsNullOrEmpty(nombreCliente))
+            {
+                Console.WriteLine("Error: el nombre del cliente es requerido");
+                return;
+            }
+
+
+            // 2. Cálculo del precio según tipo de cancha
+            double precioPorHora;
+            if (tipoCancha == "Cancha5")
+            {
+                precioPorHora = 50;
+            }
+
+            // 3. Aplicar descuento según tipo de cliente
+
+
+            // 4. Guardar la reserva DIRECTAMENTE en SQL Server
+
+
+            // 5. Enviar notificación
+
+
+            // 6. Mostrar resumen
+
+        }
+    }
+}
